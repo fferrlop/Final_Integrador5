9c6408aadf6c01a363ba089e22a2c92ab1a79a59
fferrlop <fferrlop@myuax.com>
2024-04-15 13:02:49 +0200

    errores

--- src/main/java/AnalisisNúmerico/SumListNumeros.java
+++ src/main/java/AnalisisNúmerico/SumListNumeros.java
@@ -2,9 +2,15 @@ package AnalisisNúmerico;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 public class SumListNumeros {
     public static int sumarNumerosNaturales(int n) {
+        if (n < 0) {
+            JOptionPane.showMessageDialog(null, "El número no puede ser negativo", "Error", JOptionPane.ERROR_MESSAGE);
+            return 0;
+        }
+
         if (n <= 0) {
             return 0;
         } else {
@@ -13,6 +19,11 @@ public class SumListNumeros {
     }
 
     public static List<Long> fibonacci(int n) {
+        if (n < 0) {
+            JOptionPane.showMessageDialog(null, "El número no puede ser negativo", "Error", JOptionPane.ERROR_MESSAGE);
+            return new ArrayList<>();
+        }
+
         List<Long> sequence = new ArrayList<>();
         sequence.add(0L);
         sequence.add(1L);

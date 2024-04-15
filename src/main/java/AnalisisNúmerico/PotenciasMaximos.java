32092179c6df6f34e0ac0c26f2012deb8fadad51
fferrlop <fferrlop@myuax.com>
2024-04-15 20:03:48 +0200

    prueba para devolver el maximo

--- src/main/java/AnalisisNúmerico/PotenciasMaximos.java
+++ src/main/java/AnalisisNúmerico/PotenciasMaximos.java
@@ -1,10 +1,22 @@
 package AnalisisNúmerico;
 
+import java.util.List;
+
 public class PotenciasMaximos {
     public static int power(int base, int exponent) {
         if (exponent == 0) {
             return 1;
         }
         return base * power(base, exponent - 1);
+    }
+
+    public static int maximo(List<Integer> numeros) {
+        int maximo = numeros.get(0);
+        for (int numero : numeros) {
+            if (numero > maximo) {
+                maximo = numero;
+            }
+        }
+        return maximo;
     }
 }

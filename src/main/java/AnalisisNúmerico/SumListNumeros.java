d312bb386faf5f3b07404e1428fc560442dc68b7
fferrlop <fferrlop@myuax.com>
2024-04-15 12:27:03 +0200

    muestra el rango bien

--- src/main/java/AnalisisNúmerico/SumListNumeros.java
+++ src/main/java/AnalisisNúmerico/SumListNumeros.java
@@ -1,5 +1,10 @@
 package AnalisisNúmerico;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class SumListNumeros {
     public static int sumarNumerosNaturales(int n) {
         if (n <= 0) {
@@ -7,5 +12,14 @@ public class SumListNumeros {
         } else {
             return n + sumarNumerosNaturales(n - 1);
         }
+
+    }
+
+    public static List<Integer> generarRango(int start, int end) {
+        List<Integer> range = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+            range.add(sumarNumerosNaturales(i));
+        }
+        return range;
     }
 }

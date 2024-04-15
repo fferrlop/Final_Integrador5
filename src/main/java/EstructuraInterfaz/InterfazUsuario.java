25e508d6373f5fc5accaeae6cc19e8e779a249d4
fferrlop <fferrlop@myuax.com>
2024-04-15 12:11:35 +0200

    funciona como quiero, luego lo pongo bonito

--- src/main/java/EstructuraInterfaz/InterfazUsuario.java
+++ src/main/java/EstructuraInterfaz/InterfazUsuario.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import AnalisisGenómico.ConteoGenes;
 import AnalisisGenómico.CalculoCombinaciones;
-import AnalisisNúmerico.SumListNumeros;
 
 public class InterfazUsuario {
     public static void main(String[] args) {
@@ -17,7 +16,6 @@ public class InterfazUsuario {
 
         CardLayout cardLayout = new CardLayout();
         JPanel mainPanel = new JPanel(cardLayout);
-
 
         JPanel startPanel = new JPanel();
         JButton buttonGenes = new JButton("Contador de Genes");
@@ -38,9 +36,10 @@ public class InterfazUsuario {
         });
         startPanel.add(buttonNumerico);
 
-
         JPanel genesPanel = new JPanel();
         JTextField dnaField = new JTextField(20);
+        JLabel genesResultLabel = new JLabel();
+        JLabel combinationsResultLabel = new JLabel();
         JButton analyzeButton = new JButton("Analizar");
         analyzeButton.addActionListener(new ActionListener() {
             @Override
@@ -49,7 +48,8 @@ public class InterfazUsuario {
                     String dna = dnaField.getText();
                     int numGenes = ConteoGenes.contarGenes(dna);
                     List<String> combinaciones = CalculoCombinaciones.calcularCombinaciones(dna);
-                    JOptionPane.showMessageDialog(frame, "Número de genes: " + numGenes + "\nCodones: " + combinaciones);
+                    genesResultLabel.setText("Número de genes: " + numGenes);
+                    combinationsResultLabel.setText("Combinaciones: " + combinaciones);
                 } catch (Exception ex) {
                     JOptionPane.showMessageDialog(frame, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
                 }
@@ -58,16 +58,15 @@ public class InterfazUsuario {
         genesPanel.add(new JLabel("Introduce el patrón de ADN:"));
         genesPanel.add(dnaField);
         genesPanel.add(analyzeButton);
-
+        genesPanel.add(genesResultLabel);
+        genesPanel.add(combinationsResultLabel);
 
         JPanel numericoPanel = new JPanel();
         numericoPanel.add(new JLabel("Análisis numérico"));
 
-
         mainPanel.add(startPanel, "Inicio");
         mainPanel.add(genesPanel, "Genes");
         mainPanel.add(numericoPanel, "Numerico");
-
 
         cardLayout.show(mainPanel, "Inicio");
 

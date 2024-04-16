59c82ccd1b849c359532ee9748173151a2764ed0
fferrlop <fferrlop@myuax.com>
2024-04-16 16:55:44 +0200

    los componentes de fecha

---
+++ src/main/java/GestiónInformaciónCientifica/Fecha.java
@@ -0,0 +1,32 @@
+package GestiónInformaciónCientifica;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Fecha implements Comparable<Fecha> {
+    private int dia;
+    private int mes;
+    private int año;
+
+    public Fecha(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.año = ano;
+    }
+
+    @Override
+    public int compareTo(Fecha o) {
+        if (año != o.año) {
+            return año - o.año;
+        } else if (mes != o.mes) {
+            return mes - o.mes;
+        } else {
+            return dia - o.dia;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return dia + "/" + mes + "/" + año;
+    }
+}

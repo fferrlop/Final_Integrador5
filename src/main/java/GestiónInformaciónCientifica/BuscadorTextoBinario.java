97913c0560b2229dc307f524bd54d67db836b5cf
fferrlop <fferrlop@myuax.com>
2024-04-16 16:56:04 +0200

    cambios para que el highlighting funcione bien

--- src/main/java/GestiónInformaciónCientifica/BuscadorTextoBinario.java
+++ src/main/java/GestiónInformaciónCientifica/BuscadorTextoBinario.java
@@ -1,13 +1,20 @@
 package GestiónInformaciónCientifica;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BuscadorTextoBinario {
 
     public static int buscar(String texto, String palabra) {
         String[] palabras = texto.split("\\s+");
+        Map<String, Integer> indices = new HashMap<>();
+        for (int i = 0; i < palabras.length; i++) {
+            indices.put(palabras[i], texto.indexOf(palabras[i]));
+        }
         Arrays.sort(palabras);
-        return binarySearch(palabras, palabra, 0, palabras.length - 1);
+        int index = binarySearch(palabras, palabra, 0, palabras.length - 1);
+        return index != -1 ? indices.get(palabras[index]) : -1;
     }
 
     private static int binarySearch(String[] array, String key, int left, int right) {
